b8d1b99954bf7c96df9aeb83ccd884d0aa52abf0
馬場義弘 <babayoshihiro@YB-MBP14.local>
2022-07-13 16:10:25 +0900

    Create custom icon

--- src/components/MemoList.jsx
+++ src/components/MemoList.jsx
@@ -1,6 +1,7 @@
-import { Feather } from '@expo/vector-icons';
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+
+import Icon from './Icon';
 
 export default function MemoList() {
   return (
@@ -11,7 +12,7 @@ export default function MemoList() {
           <Text style={styles.memoListItemDate}>2022年07月01日 10:00</Text>
         </View>
         <View>
-          <Feather name="x" size={16} color="#B0B0B0" />
+          <Icon name="delete" size={24} color="#B0B0B0" />
         </View>
       </View>
 
@@ -22,7 +23,7 @@ export default function MemoList() {
             <Text style={styles.memoListItemDate}>2022年07月01日 10:00</Text>
           </View>
           <View>
-            <Feather name="x" size={16} color="#B0B0B0" />
+            <Icon name="delete" size={24} color="#B0B0B0" />
           </View>
         </View>
       </View>
@@ -33,7 +34,7 @@ export default function MemoList() {
             <Text style={styles.memoListItemDate}>2022年07月01日 10:00</Text>
           </View>
           <View>
-            <Feather name="x" size={16} color="#B0B0B0" />
+            <Icon name="delete" size={24} color="#B0B0B0" />
           </View>
         </View>
       </View>
