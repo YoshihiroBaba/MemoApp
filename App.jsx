b38ff03e46754ce0a8baecaa37a07ef1e2f4b615
馬場義弘 <babayoshihiro@YB-MBP14.local>
2022-07-17 07:54:22 +0900

    Create memoEditScreen and MemoCreateScreen

--- App.jsx
+++ App.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
-import MemoListScreens from './src/screens/MemoListScreens';
+// import MemoListScreens from './src/screens/MemoListScreens';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
+// import MemoEditScreen from './src/screens/MemoEditScreen';
+import MemoCreateScreen from './src/screens/MemoCreatScreen';
 
 export default function App() {
   return (
-    <MemoListScreens />
+    <MemoCreateScreen />
   );
 }
