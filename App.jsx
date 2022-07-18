452b7a629bd83a6fcd3185dc7857565517d487bc
馬場義弘 <babayoshihiro@YB-MBP14.local>
2022-07-18 17:21:49 +0900

    Create LogInScreen and SignUp Screen

--- App.jsx
+++ App.jsx
@@ -3,10 +3,12 @@ import React from 'react';
 // import MemoListScreens from './src/screens/MemoListScreens';
 // import MemoDetailScreen from './src/screens/MemoDetailScreen';
 // import MemoEditScreen from './src/screens/MemoEditScreen';
-import MemoCreateScreen from './src/screens/MemoCreatScreen';
+// import MemoCreateScreen from './src/screens/MemoCreatScreen';
+// import LoginScreen from './src/components/LoginScreen';
+import SignUpScreen from './src/components/SignUpScreen';
 
 export default function App() {
   return (
-    <MemoCreateScreen />
+    <SignUpScreen />
   );
 }
